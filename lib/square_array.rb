29e28c4bbbee7df6da7bc6b4dd9486cff8d04606
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 22:43:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,10 @@ def square_array(array)
   new_array = []
   counter = 0 
   while counter < array.length 
-  
-  
+  new_array.push(array[counter] ** 2)
   
   counter +=1
   end
   
-  
+new_array
 end
